58707a6abb2f88904b1db582b67113d29585ee7c
Yasir Imran <yasirimrandev@gmail.com>
2026-06-11 11:48:28 +0500

    Add dotenv support and make DATABASE_URL configurableE
    Load environment variables from .env file and update DATABASE_URL to be configurable.

--- config/config.py
+++ config/config.py
@@ -1,9 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from dotenv import load_env
+import os
+load_env()
 # All configrations
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 class Settings(BaseSettings):
     # JWT Configuration
-    DATABASE_URL : str = "postgresql+asyncpg://neondb_owner:npg_jkr1NJvtZ2uR@ep-misty-smoke-apt068uz-pooler.c-7.us-east-1.aws.neon.tech/neondb"
+    DATABASE_URL : str | None = DATABASE_URL 
     SECRET_KEY : str = "my_n@me_y@asir.?"
     ALGORITHM : str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES : int = 30
